52aaf8dff37bcb1df94ccfbe103e3d76c8d38ab4
Rene Kuhm <rdesigncastex@gmail.com>
2023-10-03 23:53:08 -0300

    agregando switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), preact()]
 });
