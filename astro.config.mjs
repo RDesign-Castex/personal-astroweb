7fdbf8a555065880c619a23c62f49cc124aeb3fd
Rene Kuhm <rdesigncastex@gmail.com>
2023-11-27 17:33:24 -0300

    mejorando blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,19 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tecnodespegue.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-    preact(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(),  preact()]
+  // Aquí puedes agregar cualquier configuración adicional de Vite si es necesario
+  // vite: {
+  //   esbuild: {
+  //     loader: 'jsx' // Esto establece el loader por defecto para todos los archivos como JSX
+  //   },
+  // },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
